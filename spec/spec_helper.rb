c70cfb3eeaec6c86b5d95da9fffac1f0dd99a6ae
Tray Torrance <devwork@warrentorrance.com>
2012-01-19 16:02:40 -0800

    Enhance Spork support in RSpec and Cucumber

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,32 +7,34 @@ Spork.prefork do
   # need to restart spork for it take effect.
   ENV['RAILS_ENV'] ||= 'test'
 
+  require 'rails/mongoid'
+  Spork.trap_class_method( ::Rails::Mongoid, :load_models)
+
   require File.expand_path('../../config/environment', __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
+  require 'factory_girl_rails'
+  require 'database_cleaner'
+
+  # Configure DatabaseCleaner
+  DatabaseCleaner.strategy = :truncation
 
 end
 
 Spork.each_run do
-  # This code will be run each time you run your specs.
-
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
   RSpec.configure do |config|
-    # ## Mock Framework
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
+    ## Mock Framework
+    config.mock_with :rspec
 
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    #config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    ## Mix in Factory Girl
+    config.include FactoryGirl::Syntax::Methods
 
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
-    #config.use_transactional_fixtures = false
+    ## Clean Database entries
+    config.before( :each ) { DatabaseCleaner.clean }
 
     # If true, the base class of anonymous controllers will be inferred
     # automatically. This will be the default behavior in future versions of
