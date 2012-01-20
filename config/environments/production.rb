9caa826afe068bbd3b84132cb93bed77a5b65b24
Tray Torrance <devwork@warrentorrance.com>
2012-01-19 19:42:09 -0800

    Use YUI for JS & CSS compression in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,9 @@ MongoStalker::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress       = true
+  config.assets.js_compressor  = :yui
+  config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
